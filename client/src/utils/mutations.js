e415e64ba1056c012fd68add9cef54a115c17b14
Parker Jones <parker.matthewjones@gmail.com>
2021-09-28 01:41:06 -0500

    more attempts

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,48 +3,48 @@ import gql from 'graphql-tag';
 export const ADD_USER = gql`
     mutation addUser($username: String!, $email: String!, $password: String!) {
         addUser(username: $username, email: $email, password: $password) {
-            token
-                user {
-                    _id
-                    username
-                    email
-                    bookCount
-                    savedBooks {
-                        bookId
-                        title
-                        description
-                        authors
-                        image
-                        link
-                    }
+        token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    title
+                    description
+                    authors
+                    image
+                    link
                 }
+            }
         }
     }
 `;
 
-export const LOGIN_USER = gql `
+export const LOGIN_USER = gql`
     mutation login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
-            token
-                user {
-                    _id
-                    username
-                    email
-                    bookCount
-                    savedBooks {
-                        bookId
-                        title
-                        description
-                        authors
-                        link
-                        image
-                    }
-                }
-        }
-    }
+        token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                    bookId
+                    title
+                    description
+                    authors
+                    link
+                    image
+         }
+       }
+     }
+   }
 `;
 
-export const SAVE_BOOK = gql `
+export const SAVE_BOOK = gql`
     mutation saveBook($input: bookInput!) {
         saveBook(input: $input) {
             _id
